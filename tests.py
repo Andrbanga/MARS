81e3b43a2510ae22a5a9d3f7923cfeda5e52de27
Ramapr <arcabuceroma79@gmail.com>
2020-01-09 21:24:23 +0300

    Create tests.py
    файл для простого тестирования, логики работы классов: mobile_robot, graph , Nav_graph

---
+++ tests.py
@@ -0,0 +1,20 @@
+from robot import MobileRobot as MB 
+from graph import Graph, Nav_Graph
+
+
+
+
+map_names = ['one_line_map.npy', 'hard_lines.npy']
+
+
+start_point = [(), ()]
+
+end_point  = [(), ()]
+
+
+resulting_graph = []
+
+resulting_nav_graph = []
+
+
+
